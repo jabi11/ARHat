30e3f6d0f6bb6f78872959f287c5c1f253642efb
Jan Biernacki <jas.biernacki@gmail.com>
2019-09-03 01:14:21 +0200

    pod removal new menu

---
+++ ARHat/Model/MenuCell.swift
@@ -0,0 +1,18 @@
+//
+//  MenuCell.swift
+//  ARHat
+//
+//  Created by Jan Biernacki on 03/09/2019.
+//  Copyright © 2019 Dress Me. All rights reserved.
+//
+
+import UIKit
+
+class MenuCellClass: UICollectionViewCell {
+    
+    @IBOutlet weak var HatImage: UIImageView!
+    
+    @IBOutlet weak var HatName: UILabel!
+    
+    @IBOutlet weak var PriceLabel: UILabel!
+}
--- ARHat/View Controllers/ProductViewController.swift
+++ ARHat/View Controllers/ProductViewController.swift
@@ -7,89 +7,58 @@
 //
 
 import Foundation
-import VerticalCardSwiper
+import UIKit
 
-class ProductViewController: UIViewController, VerticalCardSwiperDatasource, VerticalCardSwiperDelegate {
+class ProductViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
-    private var cardSwiper: VerticalCardSwiper!
     
+    @IBOutlet var CollectionView: UICollectionView!
     private var currentModel: String = ""
     
     private let hats: [Hat] = [
-        Hat(name: "Cap", price: 69.69, image: UIImage(named: "hat1")!, usdzName: "capdobre1"),
-        Hat(name: "Beanie wool", price: 6.66, image: UIImage(named: "hat2")!, usdzName: "BEANIEDOBRE1")
+        Hat(name: "Cap", price: 69.69, image: UIImage(named: "cap")!, usdzName: "capdobre1"),
+        Hat(name: "Beanie wool", price: 6.66, image: UIImage(named: "beanie")!, usdzName: "BEANIEDOBRE1")
     ]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //let viewFrame = self.view.alignmentRect(forFrame: self.view.bounds)
+        CollectionView.delegate = self
+        CollectionView.dataSource = self
         
-        let viewFrame = CGRect(x: 0.0, y: 64.0, width: 375, height: 603)
-        
-        cardSwiper = VerticalCardSwiper(frame: viewFrame)
-        view.addSubview(cardSwiper)
-        view.backgroundColor = UIColor.white
-        
-        cardSwiper.datasource = self
-        cardSwiper.delegate = self
-        
-        // register cardcell for storyboard use
-        cardSwiper.register(nib: UINib(nibName: "ExampleCell", bundle: nil), forCellWithReuseIdentifier: "ExampleCell")
     }
     
-    func cardForItemAt(verticalCardSwiperView: VerticalCardSwiperView, cardForItemAt index: Int) -> CardCell {
-        
-        if let cardCell = verticalCardSwiperView.dequeueReusableCell(withReuseIdentifier: "ExampleCell", for: index) as? ExampleCardCell {
-            
-            cardCell.nameLabel.text = String(hats[index].name)
-            cardCell.priceLabel.text = String(hats[index].price)
-            cardCell.productImage.image = hats[index].image
-            cardCell.setRandomBackgroundColor()
-            
-            cardCell.callback = {
-                addToCart(item: self.hats[index])
-            }
-            
-            return cardCell
-        }
-        
-        return CardCell()
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return hats.count
     }
     
-    func didTapCard(verticalCardSwiperView: VerticalCardSwiperView, index: Int) {
-        currentModel = hats[index].usdzName
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MenuCell", for: indexPath) as! MenuCellClass
+        
+        cell.HatImage.image = hats[indexPath.row].image
+        cell.HatName.text = String(hats[indexPath.row].name)
+        cell.PriceLabel.text = String(hats[indexPath.row].price)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        currentModel = hats[indexPath.row].usdzName
         performSegue(withIdentifier: "menuToAR", sender: self)
     }
+    
+   /* func didTapCard(verticalCardSwiperView: VerticalCardSwiperView, index: Int) {
+        currentModel = hats[index].usdzName
+        performSegue(withIdentifier: "menuToAR", sender: self)
+    } */
     
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let vc = segue.destination as? FaceDetectViewController
         vc?.model = currentModel
     }
-    
-    func numberOfCards(verticalCardSwiperView: VerticalCardSwiperView) -> Int {
-        return hats.count
-    }
-    
-    
-    @IBAction func buttonTapped(_ sender: UIBarButtonItem) {
-        
-        performSegue(withIdentifier: "toAR", sender: self)
-        
-    }
-    
-   
-    @IBAction func cartTapped(_ sender: UIBarButtonItem) {
-        
-        performSegue(withIdentifier: "menuToCart", sender: self)
-        
-    }
-    
-    public func getCurrentIndex() -> Void {
-        print(String(cardSwiper.focussedCardIndex!))
-    }
+ 
     
     
 }
